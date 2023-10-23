79f1d9c9d50d8e78997fedb72188bacd50a92b47
rahul-9340 <142489789+rahul-9340@users.noreply.github.com>
2023-10-23 11:54:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+
+let output = document.getElementById("output")
+
+
+let p1 = new Promise((resolve,reject)=>{
+let condition = true;
+	if(condition){
+		setTimeout(()=>{
+	resolve("Hello, world!")
+   },1000)	
+	}
+	else{
+reject("error occured")
+}
+})
+
+
+p1.then((data)=>{
+	output.innerText = data;
+}).catch((data)=>{
+console.log(data);
+})
+
+
+
+
+
+
+
+
+
